1c34a818e4a7deac98b3e6d1be2a47d47d5e9d75
Isaac Peña <isaac@repson.org>
2019-03-25 23:09:09 +0100

    Added line in order to remove mongoose log in the terminal

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3977;
+
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/mini_spotify', (err, res) => {
     if(err){
         throw err;
