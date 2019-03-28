346293dc5ebe82e34f19b84d5c8be1755f6b8014
Isaac Peña <isaac@repson.org>
2019-03-28 23:07:35 +0100

    Minor change

--- controllers/artist.js
+++ controllers/artist.js
@@ -2,7 +2,7 @@
 
 var path = require('path');
 var fs = require('fs');
-var mongoosePaginate = require('mongoose-pagination')
+var mongoosePaginate = require('mongoose-pagination');
 var Artist = require('../models/artist');
 var Album = require('../models/album');
 var Song = require('../models/song');
