199ab2080696924b38b77929f66e5695a8c4da4b
Isaac Peña <isaac@repson.org>
2019-03-30 00:29:13 +0100

    Added getSongFile and uploadFile functions

--- controllers/song.js
+++ controllers/song.js
@@ -109,10 +109,54 @@ function deleteSong(req, res){
     });
 }
 
+function uploadFile(req, res){
+    var songId = req.params.id;
+    var file_name = 'Not uploaded';
+
+    if(req.files){
+        var file_path = req.files.file.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if(file_ext == 'mp3' || file_ext == 'ogg'){
+
+            Song.findByIdAndUpdate(songId, {file: file_name}, (err, songUpdated) => {
+                if(!songUpdated){
+                    res.status(404).send({message: 'Cannot update the song'});
+                }else{
+                    res.status(200).send({song: songUpdated});
+                }
+            });
+        }else{
+            res.status(404).send({message: 'Incorrect file extension'});
+        }
+    }else{
+        res.status(404).send({message: 'Image missed'});
+    }
+}
+
+function getSongFile(req, res){
+    var imageFile = req.params.songFile;
+    var path_file = './uploads/songs /'+imageFile;
+
+    fs.exists(path_file, function(exists){
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else{
+            res.status(200).send({message: 'Audio file missed'});
+        }
+    });
+}
+
 module.exports = {
     getSong,
     getSongs,
     saveSong,
     updateSong,
-    deleteSong
+    deleteSong,
+    uploadFile,
+    getSongFile
 };
