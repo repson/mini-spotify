df3bac69040913a89dcbbbe9a4ef6565caa69f70
Isaac Peña <isaac@repson.org>
2019-03-29 22:52:21 +0100

    Added saveAlbum function

--- controllers/album.js
+++ controllers/album.js
@@ -11,6 +11,30 @@ function getAlbum(req, res){
     res.status(200).send({message: 'getAlbum action'});
 }
 
+function saveAlbum(req, res){
+    var album = new Album();
+
+    var params = req.body;
+    album.title = params.title;
+    album.description = params.description;
+    album.year = params.year;
+    album.image = 'null';
+    album.artist = params.artist;
+
+    album.save((err, albumStored) => {
+        if(err){
+            res.status(500).send({message: 'Server error'});
+        }else{
+            if(!albumStored){
+                res.status(404).send({message: 'The album has not been saved'});
+            }else{
+                res.status(200).send({message: albumStored});
+            }
+        }
+    });
+}
+
 module.exports = {
-    getAlbum
+    getAlbum,
+    saveAlbum
 };
