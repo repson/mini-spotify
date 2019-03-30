931633a34a6210f87ee35a7f7a78ce2d17a918c3
Isaac Peña <isaac@repson.org>
2019-03-30 12:57:55 +0100

    Added UserService class

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [UserService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public title = 'Musify';
   public user: User;
   public identity;
   public token;
 
-  constructor(){
+  constructor(
+    private _userService:UserService
+  ){
     this.user = new User('', '', '', '', '', 'ROLE_USER', '');
   }
 
+  ngOnInit(){
+    var text = this._userService.signup();
+    console.log(text);
+  }
+
+  public onSubmit(){
+    console.log(this.user);
+  }
 }
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable} from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { GLOBAL } from './global';
+
+@Injectable()
+export class UserService{
+
+    public url: string;
+
+    constructor(private _http: Http){
+        this.url = GLOBAL.url;
+    }
+
+    signup(){
+        return 'Hello word';
+    }
+
+}
