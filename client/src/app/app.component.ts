1fe21b9d25775d18cda4d7eec6ad16009b001310
Isaac Peña <isaac@repson.org>
2019-04-02 22:53:23 +0200

    Logout is ok

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -85,4 +85,12 @@ export class AppComponent implements OnInit{
       }
     );
   }
+
+  logout(){
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+  }
 }
