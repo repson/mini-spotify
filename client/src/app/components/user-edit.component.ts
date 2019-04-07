58750f24b5d92cb13e8534053ced29ad7db9d6c7
Isaac Peña <ipena@wtelecom.es>
2019-04-07 19:25:20 +0200

    fileChangeEvent and makeFileRequest methods added

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import{ GLOBAL } from '../services/global';
 import { UserService } from '../services/user.service';
 import { User } from '../models/user';
+import { reject } from 'q';
 
 @Component({
     selector: 'user-edit',
@@ -15,6 +17,7 @@ export class UserEditComponent implements OnInit{
     public identity;
     public token;
     public alertMessage;
+    public url = GLOBAL.url;
 
     constructor(
         private _userService: UserService
@@ -41,6 +44,19 @@ export class UserEditComponent implements OnInit{
                     localStorage.setItem('identity', JSON.stringify(this.user));
                     document.getElementById("identity_name").innerHTML = this.user.name;
 
+                    if(!this.filesToUpload){
+                        // Redirect
+                    }else{
+                        this.makeFileRequest(this.url + 'upload-image-user/' + this.user_id, [], this.filesToUpload).then(
+                            (result: any) => {
+                                this.user.image = result.image;
+                                localStorage.setItem('identity', JSON.stringify(this.user));
+
+                                console.log(this.user);
+                            }
+                        );
+                    }
+
                     this.alertMessage = 'The user is updated successfully';
                 }
             },
@@ -55,5 +71,38 @@ export class UserEditComponent implements OnInit{
                 }
             }
         );
+    }
+
+    public filesToUpload: Array<File>;
+
+    fileChangeEvent(fileInput: any){
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+        console.log(this.filesToUpload);
+    }
+
+    makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+        var token = this.token;
+
+        return new Promise(function(resolve, reject){
+            var formData: any = new FormData();
+            var xhr = new XMLHttpRequest();
+
+            for(var i = 0; i < FileList.length; i++){
+                formData.append('image', files[i], files[i].name);
+            }
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState == 4){
+                    if(xhr.status == 200){
+                        resolve(JSON.parse(xhr.response));
+                    }else{
+                        reject(xhr.response);
+                    }
+                }
+            }
+
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Authorization', token);
+            xhr.send(formData);
+        });
     }
 }
