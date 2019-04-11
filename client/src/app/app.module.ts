60d90618b138aed0c6b03054dc5b26ec2c3d8379
Isaac Peña <isaac@repson.org>
2019-04-11 22:46:32 +0200

    Added artist-list.component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing, appRoutingProviders } from './app.routing';
+
 import { AppComponent } from './app.component';
 import { UserEditComponent } from './components/user-edit.component';
+import { ArtistListComponent } from './components/artist-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserEditComponent
-  ],
+    UserEditComponent,
+    ArtistListComponent
+   ],
   imports: [
     BrowserModule,
     FormsModule,
---
+++ client/src/app/components/artist-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit} from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { GLOBAL } from '../services/global';
+import { UserService } from '../services/user.service';
+
+@Component({
+    selector: 'artist-list',
+    templateUrl: '../views/artist-list.html',
+    providers: [UserService]
+})
+
+export class ArtistListComponent implements OnInit{
+    public title:string;
+    public artists:Artist[];
+    public identity;
+    public token;
+    public url:string;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _userService: UserService
+    ){
+        this.title = 'Artists';
+        this.identity = this._userService.getIdentity();
+        this.token = this._userService.getToken();
+        this.url = GLOBAL.url;
+    }
+
+    ngOnInit(){
+        console.log('artist-list.component.ts loaded');
+
+        // Get the artists list
+    }
+}
